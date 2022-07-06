8d026ce4400d22a811a98c2f6d778c15e999c48e
Nick McKinnon <nick89.mck@gmail.com>
2022-07-06 13:56:02 -0400

    Adds quizzesSlice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import quizzesSlice from "../features/quizzes/quizzesSlice";
 import topicSlice from "../features/topics/topicSlice";
 
 export default configureStore({
   reducer: {
-    topics: topicSlice
+    topics: topicSlice,
+    quizzes: quizzesSlice
   },
 });
