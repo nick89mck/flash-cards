349e06372d093094d1b28692be3acb9a37abf55a
Nick McKinnon <nick89.mck@gmail.com>
2022-07-06 13:30:16 -0400

    Creates quizzesSlice, with an action to add quiz, and exports reducer and selector action

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const quizzesSlice = createSlice(
+	{
+		name: 'quizzes',
+		initialState: {
+			quizzes: {}
+		},
+		reducers: {
+			addQuiz: (state, action) => {
+				const { id, name, topicId, cardIds } = action.payload;
+				state.quizzes[id] = {
+					id: id,
+					name: name,
+					topicId: topicId,
+					cardIds: cardIds
+				}
+			}
+		}
+	}
+)
+
+export const selectQuizzes = (state) =>  state.quizzes.quizzes;
+export const { addQuiz } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
